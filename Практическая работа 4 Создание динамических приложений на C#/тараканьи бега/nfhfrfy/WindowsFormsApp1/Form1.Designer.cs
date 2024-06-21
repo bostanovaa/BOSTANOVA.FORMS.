06f5357486e3aa8c119d21c2d7f6232670efb7a0
bostanovaa <168176646+bostanovaa@users.noreply.github.com>
2024-06-21 21:43:10 +0300

    Add files via upload

---
+++ Практическая работа 4 Создание динамических приложений на C#/тараканьи бега/nfhfrfy/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tarakan2 = new System.Windows.Forms.PictureBox();
+            this.tarakan1 = new System.Windows.Forms.PictureBox();
+            this.Cnfhn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tarakan2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tarakan1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tarakan2
+            // 
+            this.tarakan2.Image = ((System.Drawing.Image)(resources.GetObject("tarakan2.Image")));
+            this.tarakan2.Location = new System.Drawing.Point(40, 45);
+            this.tarakan2.Name = "tarakan2";
+            this.tarakan2.Size = new System.Drawing.Size(105, 115);
+            this.tarakan2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tarakan2.TabIndex = 0;
+            this.tarakan2.TabStop = false;
+            this.tarakan2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tarakan1
+            // 
+            this.tarakan1.Image = ((System.Drawing.Image)(resources.GetObject("tarakan1.Image")));
+            this.tarakan1.Location = new System.Drawing.Point(33, 256);
+            this.tarakan1.Name = "tarakan1";
+            this.tarakan1.Size = new System.Drawing.Size(115, 122);
+            this.tarakan1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tarakan1.TabIndex = 1;
+            this.tarakan1.TabStop = false;
+            this.tarakan1.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Cnfhn
+            // 
+            this.Cnfhn.BackColor = System.Drawing.Color.White;
+            this.Cnfhn.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Cnfhn.Location = new System.Drawing.Point(341, 648);
+            this.Cnfhn.Name = "Cnfhn";
+            this.Cnfhn.Size = new System.Drawing.Size(146, 58);
+            this.Cnfhn.TabIndex = 2;
+            this.Cnfhn.Text = "CТАРТ";
+            this.Cnfhn.UseVisualStyleBackColor = false;
+            this.Cnfhn.Click += new System.EventHandler(this.Cnfhn_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(206, 661);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "СБРОС";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.tarakan1);
+            this.panel1.Controls.Add(this.tarakan2);
+            this.panel1.Location = new System.Drawing.Point(3, 173);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 430);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(67, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Георгий";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(206, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Геннадий";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(800, 761);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Cnfhn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tarakan2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tarakan1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox tarakan2;
+        private System.Windows.Forms.PictureBox tarakan1;
+        private System.Windows.Forms.Button Cnfhn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Практическая работа 4 Создание динамических приложений на C#/тараканьи бега/nfhfrfy/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int flag1;
+        int x1, x2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Cnfhn_Click(object sender, EventArgs e)
+        {
+            flag1 = 1;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            label2.Text = "0";
+            x1 = 1; x2 = 1;
+            tarakan1.Left = x1;
+            tarakan2.Left = x2;
+            flag1 = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Георгий";
+            textBox2.Text = "Геннадий";
+            label1.Text = "0";
+            label2.Text = "0";
+            tarakan1.Left = 1;
+            tarakan2.Left = 1;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (flag1 != 0)
+            {
+                Random a = new Random(); 
+                int count = a.Next(8);
+                x1 += count;
+                label1.Text = Convert.ToString(x1);
+                tarakan1.Left = x1;
+                count = a.Next(8);
+                x2 += count;
+                label2.Text = Convert.ToString(x2);
+                tarakan2.Left = x2;
+            }
+            if ((x1 >= 680) || (x2 >= 680))
+            {
+                flag1 = 0;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
